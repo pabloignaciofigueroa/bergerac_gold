391f51f1f8e6b7e667bab68441753789b99fad0a
Pablo Figueroa <pabloignaciofigueroa@gmail.com>
2026-09-13 03:44:05 -0300

    Revert "Publicar estructura SEO local, servicios, casos y guias de Bergerac"
    This reverts commit 004cf0b77bb526e4feefb31d68f9e7856aa3b5b7.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import { pages } from './src/content/seo-pages.js';
 
 export default defineConfig({
-  base: '/',
+  base: './',
   server: { port: 5173, strictPort: true },
-  build: { target: 'es2022', rolldownOptions: { input: ['index.html','bergerac.html','404.html',...pages.map(page=>`.${page.path}index.html`)] } },
+  build: { target: 'es2022', rolldownOptions: { input: ['index.html','bergerac.html'] } },
 });
